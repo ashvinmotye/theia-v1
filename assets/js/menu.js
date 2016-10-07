327bcb275bb1f2a348faf77911722edfdbfe1349
ashvintest <motye13@gmail.com>
2016-10-07 15:18:50 +0400

    Menu button

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,13 +3,20 @@ toggleMenu();
 function toggleMenu() {
 	var button = document.getElementById("menu-nav");
 	var menu = document.getElementById("menu");
+	var bar1 = document.getElementById("bar1");
+	var bar2 = document.getElementById("bar2");
+	var bar3 = document.getElementById("bar3");
 
 	button.addEventListener("click", function(){
 		menu.classList.toggle("menu-toggle");
-		button.textContent = "X";
+		bar1.classList.toggle("close");
+		bar2.classList.toggle("close");
+		bar3.classList.toggle("close");
 
-		if (menu.className == "menu-toggle") {
-			button.textContent = "T";
+		if (menu.className != "menu-toggle") {
+			button.style.backgroundColor = "#DDDCDC";
+		} else {
+			button.style.backgroundColor = "#247EB4";
 		}
 	});
 }
